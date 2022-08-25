f350639dd8616b159356e05d62bfb345eb1f073b
perchik_air <1919ars@gmail.com>
2022-08-25 21:44:02 +0300

    more

--- impl_env_windy_gridworld.py
+++ impl_env_windy_gridworld.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 class WindyGridworldEnv:
-    def __init__(self, width=10, high=7, wind=None, mode='human', actions=4, kind="grid"):
+    def __init__(self, width=10, high=7, wind=None, mode='human', actions=4, kind="grid", episode_length=100):
         self.width = width
         self.high = high
         self.wind = wind
@@ -15,16 +15,21 @@ class WindyGridworldEnv:
         self.state = [0, 0]
         self.goal = [7, 3]
         self.counter = 0
+        self.last_counter = 0
+        self.episode_length = episode_length
         self.map = np.zeros((self.width, self.high))
         if self.wind is None:
             self.wind = [0, 0, 0, 1, 1, 1, 2, 2, 1, 0]
         if self.kind == 'grid':
             self.actions = 4
+        if self.kind == 'king':
+            self.actions = 8
         if self.mode == 'human':
             self.path_x = []
             self.path_y = []
 
     def reset(self):
+        self.last_counter = self.counter
         self.counter = 0
         for i in range(self.width):
             wind_strength = self.wind[i]
@@ -59,6 +64,41 @@ class WindyGridworldEnv:
 
         self.state = [curr_x, curr_y]
 
+    def king_action(self, action):
+        curr_x, curr_y = self.state
+        if action == 0:
+            # Up
+            curr_y = min(self.high - 1, curr_y + 1)
+        elif action == 1:
+            # Up + right
+            curr_y = min(self.high-1, curr_y + 1)
+            curr_x = min(self.width - 1, curr_x + 1)
+        elif action == 2:
+            # right
+            curr_x = min(self.width-1, curr_x + 1)
+        elif action == 3:
+            # down + right
+            curr_y = max(0, curr_y - 1)
+            curr_x = min(self.width-1, curr_x + 1)
+        elif action == 4:
+            # down
+            curr_y = max(0, curr_y - 1)
+        elif action == 5:
+            # down + left
+            curr_y = max(0, curr_y - 1)
+            curr_x = max(0, curr_x - 1)
+        elif action == 6:
+            # left
+            curr_x = max(0, curr_x - 1)
+        elif action == 7:
+            # up + left
+            curr_y = min(self.high - 1, curr_y + 1)
+            curr_x = max(0, curr_x - 1)
+        else:
+            raise RuntimeError('action is incorrect')
+
+        self.state = [curr_x, curr_y]
+
     def wind_correction(self):
         curr_x, curr_y = self.state
         wind_strength = self.wind[curr_x]
@@ -71,18 +111,26 @@ class WindyGridworldEnv:
         # change state
         if self.kind == 'grid':
             self.grid_action(action)
+        elif self.kind == 'king':
+            self.king_action(action)
+        else:
+            raise RuntimeError('no kind')
 
         self.wind_correction()
 
-        # change reward
+        # change reward and check if done
+        self.counter += 1
+
         if self.state == self.goal:
             reward = 1
+            done = True
         else:
+            done = self.counter == self.episode_length
             reward = -1
 
-        # check if done
-        self.counter += 1
-        done = self.counter == 10000
+        # check for error
+        if self.counter > self.episode_length:
+            raise RuntimeError('too long')
 
         # for render
         if self.mode == 'human':
@@ -100,6 +148,7 @@ class WindyGridworldEnv:
             plt.imshow(self.map.T, origin='lower')
             plt.plot(self.path_x, self.path_y, c='red', alpha=0.7, linewidth=5.5)
             plt.scatter(self.state[0], self.state[1], s=100, c='red')
+            plt.scatter(self.goal[0], self.goal[1], s=100, c='blue')
             plt.pause(0.05)
 
 
---
+++ impl_q_learning.py
@@ -0,0 +1,70 @@
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from impl_env_windy_gridworld import WindyGridworldEnv
+
+
+def epsilon_greedy(q_func, state, actions):
+    if random.random() > EPSILON:
+        actions_dict = {action: q_func[state[0], state[1], action] for action in actions}
+        action = max(actions_dict, key=actions_dict.get)
+        return action
+
+    action = random.choice(actions)
+    return action
+
+
+def run_q_learning(q_func, env):
+    times = 100000
+    actions = env.action_values()
+    episodes = 1
+    total_return = 0
+    state = env.reset()
+    for i in range(times):
+        action = epsilon_greedy(q_func, state, actions)
+        next_state, reward, done = env.step(action)
+
+        # Learning
+        if not done:
+            actions_dict = {action: q_func[next_state[0], next_state[1], action] for action in actions}
+            action = max(actions_dict, key=actions_dict.get)
+            max_q = q_func[next_state[0], next_state[1], action]
+            new_q = q_func[state[0], state[1], action] + ALPHA * (reward + GAMMA * max_q - q_func[state[0], state[1], action])
+            q_func[state[0], state[1], action] = new_q
+        else:
+            new_q = q_func[state[0], state[1], action] + ALPHA * (reward - q_func[state[0], state[1], action])
+            q_func[state[0], state[1], action] = new_q
+
+        # render + metrics
+        if i % 20 == 0 or episodes > 250:
+            env.render()
+
+        total_return += reward
+        print(f'\r[iter {i}, episode {episodes} step {env.last_counter}] state: {state}, reward: {total_return}, done: {done}',
+              end='')
+
+        if not done:
+            state = next_state
+        else:
+            # End of episode
+            episodes += 1
+            total_return = 0
+            state = env.reset()
+            print()
+
+    plt.show()
+
+
+def main():
+    # env = WindyGridworldEnv()
+    # env = WindyGridworldEnv(kind='king', episode_length=200)
+    env = WindyGridworldEnv(episode_length=200)
+    q_func = np.zeros((env.width, env.high, env.actions))
+    run_q_learning(q_func, env)
+
+
+if __name__ == '__main__':
+    EPSILON = 0.1
+    ALPHA = 0.5
+    GAMMA = 0.9
+    main()
